5e01e7c262724a6bf9d0c10035fa3f48e7a46ec8
Adam Saponara <as@php.net>
2018-09-28 11:56:01 -0400

    Tighten up signal handling in pgrep mode

--- pgrep.c
+++ pgrep.c
@@ -2,45 +2,49 @@
 
 static int wait_for_turn(char producer_or_consumer);
 static void pgrep_for_pids();
-static void *work(void *arg);
+static void *run_work_thread(void *arg);
 static int is_already_attached(int pid);
-static void init_threads();
-static void init_signal_handler();
+static void init_work_threads();
+static void deinit_work_threads();
+static int block_all_signals();
 static void handle_signal(int signum);
+static void *run_signal_thread(void *arg);
 
 static int *avail_pids = NULL;
 static int *attached_pids = NULL;
-static pthread_t *work_thread = NULL;
+static pthread_t *work_threads = NULL;
+static pthread_t signal_thread;
 static int avail_pids_count = 0;
 static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 static pthread_cond_t can_produce = PTHREAD_COND_INITIALIZER;
 static pthread_cond_t can_consume = PTHREAD_COND_INITIALIZER;
+static int done_pipe[2];
 
 int main_pgrep() {
     long i;
 
     if (opt_num_workers < 1) {
-        fprintf(stderr, "Expected max concurrent workers (-N) > 0\n");
+        fprintf(stderr, "Expected max concurrent workers (-T) > 0\n");
         exit(1);
     }
 
-    init_signal_handler();
-    init_threads();
+    pthread_create(&signal_thread, NULL, run_signal_thread, NULL);
+    block_all_signals();
+
+    init_work_threads();
 
     for (i = 0; i < opt_num_workers; i++) {
-        pthread_create(&work_thread[i], NULL, work, (void*)i);
-        // pthread_detach(work_thread[i]);
+        pthread_create(&work_threads[i], NULL, run_work_thread, (void*)i);
     }
 
     pgrep_for_pids();
 
     for (i = 0; i < opt_num_workers; i++) {
-        pthread_join(work_thread[i], NULL);
+        pthread_join(work_threads[i], NULL);
     }
+    pthread_join(signal_thread, NULL);
 
-    free(avail_pids);
-    free(attached_pids);
-    free(work_thread);
+    deinit_work_threads();
 
     fprintf(stderr, "main_pgrep finished gracefully\n");
     return 0;
@@ -76,6 +80,7 @@ static void pgrep_for_pids() {
     char line[64];
     int pid;
     int found;
+    struct timespec timeout;
     if (asprintf(&pgrep_cmd, "pgrep %s", opt_pgrep_args) < 0) {
         errno = ENOMEM;
         perror("asprintf");
@@ -93,14 +98,23 @@ static void pgrep_for_pids() {
             found += 1;
         }
         pclose(pcmd);
-        if (found > 0) pthread_cond_broadcast(&can_consume);
+        if (found > 0) {
+            pthread_cond_broadcast(&can_consume);
+        } else {
+            clock_gettime(CLOCK_REALTIME, &timeout);
+            timeout.tv_sec += 2;
+            pthread_cond_timedwait(
+                &can_produce,
+                &mutex,
+                &timeout
+            );
+        }
         pthread_mutex_unlock(&mutex);
-        if (found < 1) sleep(2); // TODO select/cond_wait instead of blind sleep
     }
     free(pgrep_cmd);
 }
 
-static void *work(void *arg) {
+static void *run_work_thread(void *arg) {
     int worker_num;
     worker_num = (long)arg;
     while (!done) {
@@ -126,11 +140,11 @@ static int is_already_attached(int pid) {
     return 0;
 }
 
-static void init_threads() {
+static void init_work_threads() {
     avail_pids = calloc(opt_num_workers, sizeof(int));
     attached_pids = calloc(opt_num_workers, sizeof(int));
-    work_thread = calloc(opt_num_workers, sizeof(pthread_t));
-    if (!avail_pids || !attached_pids || !work_thread) {
+    work_threads = calloc(opt_num_workers, sizeof(pthread_t));
+    if (!avail_pids || !attached_pids || !work_threads) {
         errno = ENOMEM;
         perror("calloc");
         exit(1);
@@ -140,21 +154,68 @@ static void init_threads() {
     pthread_cond_init(&can_consume, NULL);
 }
 
-static void init_signal_handler() {
-    // TODO Tighten up signal handling
-    struct sigaction sa = {0};
+static void deinit_work_threads() {
+    free(avail_pids);
+    free(attached_pids);
+    free(work_threads);
+    pthread_mutex_destroy(&mutex);
+    pthread_cond_destroy(&can_produce);
+    pthread_cond_destroy(&can_consume);
+}
 
-    sa.sa_handler = handle_signal;
-    sigaction(SIGINT, &sa, NULL);
-    sigaction(SIGTERM, &sa, NULL);
-
-    sa.sa_handler = SIG_IGN;
-    sigaction(SIGPIPE, &sa, NULL);
+static int block_all_signals() {
+    int rv;
+    sigset_t set;
+    try(rv, sigfillset(&set));
+    try(rv, sigprocmask(SIG_BLOCK, &set, NULL));
+    return 0;
 }
 
 static void handle_signal(int signum) {
-    //int w;
-    (void)signum;
-    //w = write(STDERR_FILENO, "\nmain_pgrep caught signal\n", sizeof("\nmain_pgrep caught signal\n"));
+    int rv;
+    fcntl(done_pipe[1], F_SETFL, O_NONBLOCK);
+    rv = write(done_pipe[1], &signum, sizeof(int));
+    (void)rv;
+}
+
+static void *run_signal_thread(void *arg) {
+    int rv;
+    int signum;
+    fd_set rfds;
+    struct timeval tv;
+    struct sigaction sa;
+
+    (void)arg;
+
+    /* Create done_pipe */
+    rv = pipe(done_pipe);
+
+    /* Install signal handler */
+    memset(&sa, 0, sizeof(struct sigaction));
+    sa.sa_handler = handle_signal;
+    sigaction(SIGINT, &sa, NULL);
+    sigaction(SIGTERM, &sa, NULL);
+    sa.sa_handler = SIG_IGN;
+    sigaction(SIGPIPE, &sa, NULL);
+
+    /* Wait for write on done_pipe from handle_signal */
+    do {
+        FD_ZERO(&rfds);
+        FD_SET(done_pipe[0], &rfds);
+        tv.tv_sec = 1;
+        tv.tv_usec = 0;
+        rv = select(done_pipe[0]+1, &rfds, NULL, NULL, &tv);
+    } while (rv < 1);
+
+    /* Read pipe for fun */
+    rv = read(done_pipe[0], &signum, sizeof(int));
+
+    /* Set done flag; wake up all threads */
     done = 1;
+    pthread_mutex_lock(&mutex);
+    pthread_cond_broadcast(&can_consume);
+    pthread_cond_broadcast(&can_produce);
+    pthread_mutex_unlock(&mutex);
+
+    return NULL;
 }
