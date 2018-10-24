b4ed61bf32de7924e1af585ff0edb8a08033420f
Adam Saponara <as@php.net>
2018-10-24 16:54:40 -0400

    Fix test for <7.2 (#30)

--- tests/test.sh
+++ tests/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-[ -z "$phpv" ] && phpv=$(php -r '$v=PHP_VERSION; echo $v{0} . $v{2};')
+[ -z "$phpv" ] && phpv=$($PHP -r '$v=PHP_VERSION; echo $v{0} . $v{2};')
 
 actual=$(
     $PHPSPY \
@@ -16,7 +16,7 @@ for testname in "${!expected[@]}"; do
     if grep -Pq "$expected_re" <<<"$actual"; then
         echo -e "  \x1b[32mOK \x1b[0m $testname"
     else
-        echo -e "  \x1b[31mERR\x1b[0m $testname expected=$expected_re\n\nactual=\n$actual"
+        echo -e "  \x1b[31mERR\x1b[0m $testname\nexpected=$expected_re\n\nactual=\n$actual"
         exit 1
     fi
 done
