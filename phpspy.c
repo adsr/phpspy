5deb1a31863fd090437aa032318c860f42f6646d
Adam Saponara <as@php.net>
2018-10-27 13:15:16 -0400

    Replace heap allocations in try_get_php_version

--- phpspy.c
+++ phpspy.c
@@ -19,7 +19,7 @@ uint64_t opt_trace_limit = 0;
 char *opt_path_output = "-";
 char *opt_path_child_out = "phpspy.%d.out";
 char *opt_path_child_err = "phpspy.%d.err";
-char *opt_phpv = NULL;
+char *opt_phpv = "auto";
 int opt_pause = 0;
 
 size_t zend_string_val_offset = 0;
@@ -37,7 +37,7 @@ static void get_clock_time(struct timespec *ts);
 static void calc_sleep_time(struct timespec *end, struct timespec *start, struct timespec *sleep);
 static int copy_proc_mem(trace_context_t *context, const char *what, void *raddr, void *laddr, size_t size);
 static void varpeek_add(char *varspec);
-static void try_get_php_version(char **phpv);
+static void try_get_php_version(pid_t pid);
 
 #ifdef USE_ZEND
 static int do_trace(trace_context_t *context);
@@ -226,8 +226,8 @@ int main_pid(pid_t pid) {
     #else
 
 
-    if (opt_phpv == NULL) {
-      try_get_php_version(&opt_phpv);
+    if (strcmp(opt_phpv, "auto") == 0) {
+        try_get_php_version(pid);
     }
 
     if (strcmp("70", opt_phpv) == 0) {
@@ -429,56 +429,36 @@ static int copy_proc_mem(trace_context_t *context, const char *what, void *raddr
     return rv;
 }
 
-void try_get_php_version(char **phpv) {
+void try_get_php_version(pid_t pid) {
+    char version_cmd[256];
+    char phpv_minor;
     FILE *pcmd;
-    char *which_cmd;
-    char *strings_cmd;
-    char *php_path = NULL;
-    size_t php_path_size = 4096;
-    ssize_t size;
-    char *phpv_raw = NULL;
-    size_t phpv_raw_size = 64;
 
-    if (asprintf(&which_cmd, "which php") < 0) {
-        errno = ENOMEM;
-        perror("asprintf");
+    snprintf(
+        version_cmd,
+        sizeof(version_cmd),
+        "awk 'NR==1{path=$NF} /libphp7/{path=$NF} END{print path}' /proc/%d/maps"
+        " | xargs strings -d "
+        " | grep -Po '(?<=X-Powered-By: PHP/7\\.)\\d'",
+        pid
+    );
+
+    if ((pcmd = popen(version_cmd, "r")) == NULL) {
+        perror("try_get_php_version: popen");
         return;
-    }
-
-    if ((pcmd = popen(which_cmd, "r")) != NULL) {
-        size = getline(&php_path, &php_path_size, pcmd);
-        if (size <= 0) {
-            perror("Unable to locate path to php binary");
-            return;
+    } else if (fread(&phpv_minor, sizeof(char), 1, pcmd) == 1) {
+        switch (phpv_minor) {
+            case '0': opt_phpv = "70"; break;
+            case '1': opt_phpv = "71"; break;
+            case '2': opt_phpv = "72"; break;
+            case '3': opt_phpv = "73"; break;
+            case '4': opt_phpv = "74"; break;
         }
-        php_path[size - 1] = 0;
-        pclose(pcmd);
     }
-    free(which_cmd);
-
-    if (asprintf(&strings_cmd, "strings -d %s | grep \"X-Powered-By\"", php_path) < 0) {
-        errno = ENOMEM;
-        perror("asprintf");
-        return;
+    if (opt_phpv == NULL) {
+        fprintf(stderr, "try_get_php_version: Could not detect PHP version\n");
     }
-
-    if ((pcmd = popen(strings_cmd, "r")) != NULL) {
-        size = getline(&phpv_raw, &phpv_raw_size, pcmd);
-        if (size <= 0) {
-            perror("Unable to determine php version");
-            return;
-        }
-
-        if (asprintf(phpv, "%c%c", phpv_raw[18], phpv_raw[20]) < 0) {
-            errno = ENOMEM;
-            perror("asprintf");
-            return;
-        }
-        pclose(pcmd);
-    }
-    free(strings_cmd);
-    free(php_path);
-    free(phpv_raw);
+    pclose(pcmd);
 }
 
 /* TODO figure out a way to make this cleaner */
