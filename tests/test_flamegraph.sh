7b00b9a369cfd5ac9cd49a57beabd71bf9ad5e1f
Adam Saponara <as@php.net>
2018-11-13 11:58:09 -0500

    whitespace

--- tests/test_flamegraph.sh
+++ tests/test_flamegraph.sh
@@ -3,7 +3,7 @@
 REPO=$(dirname $(dirname $TEST_SH))
 
 on_exit() { rm -f $flame_svg; }
-fail()    { echo -e "  \x1b[31mERR\x1b[0m $@"; exit 1; }
+fail()    { echo -e "  \x1b[31mERR \x1b[0m $@"; exit 1; }
 
 flame_svg=$(mktemp)
 trap on_exit EXIT
@@ -22,4 +22,4 @@ $PHPSPY -O/dev/null -E/dev/null --request-info=QCUP 2>/dev/null \
     > $flame_svg
 grep -Pq '\d+ samples' $flame_svg || fail "Failed to generate flame graph"
 
-echo -e "  \x1b[32mOK \x1b[0m flamegraph"
+echo -e "  \x1b[32mOK  \x1b[0m flamegraph"
