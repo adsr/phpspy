4241ad4bffb76089a23751d5ffcbc37f51bff3b1
Adam Saponara <as@php.net>
2019-09-19 00:37:55 -0400

    Add callgrind output mode (experimental) (#4)

--- phpspy.h
+++ phpspy.h
@@ -181,5 +181,6 @@ extern int main_top(int argc, char **argv);
 extern void usage(FILE *fp, int exit_code);
 extern int get_symbol_addr(addr_memo_t *memo, pid_t pid, const char *symbol, uint64_t *raddr);
 extern int event_handler_fout(struct trace_context_s *context, int event_type);
+extern int event_handler_callgrind(struct trace_context_s *context, int event_type);
 
 #endif
